14f97f2516c238a93f96642da2f0221a0cef57c9
pyimyozin <pyimyozin1357@gmail.com>
2023-08-20 23:18:23 +0630

    day4 lessons

--- array-test.php
+++ array-test.php
@@ -4,14 +4,16 @@ $user = [
     "name" => "John Doe",
     "age" => 30,
     "occupation" => "programmer",
-    "gratuated" => true
+    "gratuated" => false
 ];
 
 foreach($user as $key => $data) {
 
     if($key === "gratuated" && $data == true) {
         $data = "Yes";
+    } else if($key === "gratuated" && $data == false) {
+        $data = "No";
     }
-
+     
     echo $key . "=" . $data. "<br>";
 }
---
+++ looping-array.php
@@ -0,0 +1,43 @@
+<?php
+
+$numbers = [1,2,3,4,5,6,7,8];
+
+//looping array with for loop
+
+// for($number = 1; $number <= count($numbers); $number++) {
+//     echo $number . "<br>";
+// }
+
+
+// //looping array with foreach
+// foreach($numbers as $number) {
+//     echo $number . "<br>";
+// }
+
+
+// looping array with key value using foreach loop
+// foreach($numbers as $key=>$number) {
+//     echo $number . "<br>";
+// }
+
+// looping array with while loop
+// $number = 0;
+// while($number < count($numbers)) {
+//     echo $numbers[$number]."<br>";
+//     $number++;
+// }
+
+
+// looping array with do while loop
+// $number = 0;
+// do {
+//     echo $numbers[$number]."<br>";
+//     $number++;
+// }
+
+// while($number < count($numbers));
+
+
+
+
+
