329bc38b90ce0dc102dbe697d2e5a59afec9d0ee
pyimyozin <pyimyozin1357@gmail.com>
2023-08-21 16:35:15 +0630

    updated some codes

--- looping-array.php
+++ looping-array.php
@@ -1,11 +1,11 @@
 <?php
 
-$numbers = [1,2,3,4,5,6,7,8];
+$numbers = [2,3,4,5,6,7,8];
 
 //looping array with for loop
 
-// for($number = 1; $number <= count($numbers); $number++) {
-//     echo $number . "<br>";
+// for($number = 0; $number <= count($numbers); $number++) {
+//     echo $numbers[$number] . "<br>";
 // }
 
 
@@ -29,13 +29,13 @@ $numbers = [1,2,3,4,5,6,7,8];
 
 
 // looping array with do while loop
-// $number = 0;
-// do {
-//     echo $numbers[$number]."<br>";
-//     $number++;
-// }
+$number = 0;
+do {
+    echo $numbers[$number]."<br>";
+    $number++;
+}
 
-// while($number < count($numbers));
+while($number < count($numbers));
 
 
 
